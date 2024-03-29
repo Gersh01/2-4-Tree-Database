dd4d2c5af769e8d33badfcce2332dd085a4a5149
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2024-03-29 01:37:30 -0400

    feat(delete): Add transfer operation
    #Finished the transfer operation for deleting a value from a leaf node, started work on fusion operation. Fusion needs the case for multiple fusions that reach root, as well as further testing of finished fusion operation.

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@
 * 02/27/2024    Alex            Finished insert method
 * 03/25/2024    Alex            Finished getNode method
 * 03/27/2024    Alex            Finished getLeafNode methods, and set up structure for deletion
+* 03/29/2024    Alex            Finished delete by transfer, started getting fusion done
 *
 /******************************************************************/
 //TO-DO:
@@ -17,7 +18,7 @@
  * Requirements for putting into production
  *      Delete functionality
  *          - Deleting a value from an internal node
- *          - Deleting leaf node values
+ *          - Deleting a leaf node using fusion
  *      Add a menu
  *          - Insert
  *          - Delete
@@ -107,33 +108,38 @@ class Tree {
         return getLeftLeafNode(node.children.get(0), value);
     }
     public Tree getParent(int value) {
-        Tree tmpTree = new Tree();
-        ArrayList<Integer> curValues = nodeValues;
-        ArrayList<Tree> curChildren = children;
-        Tree curNode = null;
-        int position = getIndex(curValues, value);
+        Tree parentTree = new Tree();
+        parentTree.nodeValues = nodeValues;
+        parentTree.children = children;
 
-        if(curValues.contains(value)) return null;
-        //parent of the root node returns null
-        if(!curChildren.isEmpty()) {
-            boolean validParent = curChildren.get(position).children.isEmpty();
-            
-            tmpTree.nodeValues = curValues;
-            tmpTree.children = curChildren;
-            curNode = tmpTree;
+        Tree childTree = null;
+        int position;
+        
+        while(!parentTree.children.isEmpty()) {
+            position = getIndex(parentTree.nodeValues, value);
+            childTree = parentTree.children.get(position);
 
-            if(!validParent) {
-                while(!validParent) {
-                    curNode = curChildren.get(position);
-                    curValues = curNode.nodeValues;
-                    curChildren = curNode.children;
-                }
-            }
+            if(childTree.nodeValues.contains(value)) return parentTree;
+
+            parentTree = childTree;
         }
-        return curNode;
+
+        return null;
     }
    
     //insert method with helper method
+    public void rebalanceTree(Tree node) {
+        if(node == null)
+            return;
+
+        if(node.nodeValues.size() > 3) {
+            rebalanceAdd(node);
+        }
+
+        for(Tree t: node.children)
+            rebalanceTree(t);
+
+    }
     public void rebalanceAdd(Tree node) {
         //grab the third value and the parent node of that value
         int newParentVal = node.nodeValues.get(2);
@@ -204,30 +210,151 @@ class Tree {
     }
     
     //delete method with helper methods
-    public void fusion(Tree node, int value) {
+    public void fusion(Tree delNode, int value) {
+        Tree parentNode = getParent(value);
+        Tree leftSibling = null;
+        Tree rightSibling = null;
 
+        //if fusion reaches the root
+        if(parentNode == null) {
+            return;
+        }
+
+        int position = getIndex(parentNode.nodeValues, value);
+        int parentValue = parentNode.nodeValues.get(position);
+
+        if((position - 1) >= 0)
+            leftSibling = parentNode.children.get(position - 1);
+
+        if((position + 1) < parentNode.children.size())
+            rightSibling = parentNode.children.get(position + 1);
+
+        parentNode.nodeValues.remove(position);
+        delNode.nodeValues.set(0, parentValue);
+        
+        if(leftSibling != null) {
+            for(int i = 0; i < leftSibling.nodeValues.size(); i++)
+                delNode.nodeValues.add(i, leftSibling.nodeValues.get(i));
+
+            for(int i = 0; i < leftSibling.children.size(); i++)
+                delNode.children.add(i, leftSibling.children.get(i));
+
+            parentNode.children.remove(position);
+        }
+        else {
+            for(int i = 0; i < rightSibling.nodeValues.size(); i++)
+                delNode.nodeValues.add(rightSibling.nodeValues.get(i));
+
+            for(int i = 0; i < rightSibling.children.size(); i++)
+                delNode.children.add(rightSibling.children.get(i));
+
+            parentNode.children.remove(position + 1);
+        }
+
+        //when the parent also only has one value, do another fusion
+        if(parentNode.nodeValues.size() == 1) {
+            fusion(parentNode, parentValue);
+        }
     }
-    public void transfer(Tree node, int value) {
+    public boolean transfer(Tree delNode, Tree parentNode, Tree leftSibling, Tree rightSibling, int value) {
+        int siblingValue;
+        int parentValue;
+        int position;
 
+        //if the left has values to share, make this first
+        if(leftSibling != null && leftSibling.nodeValues.size() > 1) {
+            
+            //find the highest value from the left and delete it
+            position = leftSibling.nodeValues.size() - 1;
+            siblingValue = leftSibling.nodeValues.get(position);
+            leftSibling.nodeValues.remove(position);
+  
+            //find the parent value
+            position = getIndex(parentNode.nodeValues, value) - 1;
+            parentValue = parentNode.nodeValues.get(position);
+  
+            //replace with sibling value
+            parentNode.nodeValues.set(position, siblingValue);
+
+            //replace the only element in the delNode with parentValue
+            delNode.nodeValues.set(0, parentValue);
+
+            return true;
+        }
+        if(rightSibling != null && rightSibling.nodeValues.size() > 1) {
+            
+            //find the lowest value from the right and delete it
+            siblingValue = rightSibling.nodeValues.get(0);
+            rightSibling.nodeValues.remove(0);
+
+            //find the parent value
+            position = getIndex(parentNode.nodeValues, value);
+            parentValue = parentNode.nodeValues.get(position);
+
+            //replace with sibling value
+            parentNode.nodeValues.set(position, siblingValue);
+
+            //replace the only element in the delNode with parentValue
+            delNode.nodeValues.set(0, parentValue);
+
+            return true;
+        }
+
+        //both left and right siblings also have only one value
+        return false;
     }
     public void delete(Tree node, int value) {
         Tree delNode = getNode(value);
-
         int position = getIndex(delNode.nodeValues, value);
         Tree parent = getParent(value);
-        Tree leafNode = getLeftLeafNode(delNode.children.get(position), value);
+
+        //internal node deletion helper
+        Tree leafNode = null;
+
+        //leaf node deletion size > 1 helpers
+        Tree leftSibling = null;
+        Tree rightSibling = null;
 
         //tree with depth of 0
         if(parent == null && delNode.children.isEmpty()) {
             delNode.nodeValues.remove(value);
+            return;
         }
+
+        position = getIndex(parent.nodeValues, value);
+
+        //if a left sibling exists - finding the siblings first means less 
+        //calls for the parent which takes a long time
+        if((position - 1) >= 0)
+            leftSibling = parent.children.get(position - 1);
+
+        //if a right sibling exists
+        if((position + 1) < parent.children.size())
+            rightSibling = parent.children.get(position + 1);
+
         //handle for internal node deletion
-        else if(!delNode.children.isEmpty()) {
-
+        if(!delNode.children.isEmpty()) {
+            leafNode = getLeftLeafNode(delNode.children.get(position), value);
         }
-        //handle for leaf node
-        else if(parent != null && delNode.children.isEmpty()) {
 
+        //handle for leaf node
+        if(parent != null && delNode.children.isEmpty()) {
+            //if leaf node has enough nodes after deletion
+            if(delNode.nodeValues.size() > 1) {
+                delNode.nodeValues.remove(Integer.valueOf(value));
+                return;
+            }
+            
+            //check for transfer first, if transfer does not work do fusion, fusion has a chance to break the 2-4 property
+            if(!transfer(delNode, parent, leftSibling, rightSibling, value)) {
+                fusion(node, value);    
+
+                Tree tmpTree = new Tree();
+                tmpTree.nodeValues = nodeValues;
+                tmpTree.children = children;
+
+                rebalanceTree(tmpTree);
+            }
         }
     }
 
