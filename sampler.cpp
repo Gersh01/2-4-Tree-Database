ba70377491d003642510c969e0a4e5ef154814aa
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2024-04-04 22:42:25 -0400

    feat(sampler): new file output
    now outputs files with the same amount of insertions and deletions

--- sampler.cpp
+++ sampler.cpp
@@ -2,19 +2,26 @@
 #include <cstdlib>
 #include <fstream>
 
-#define NUMADD 30000
-#define NUMMAX 1000
+#define NUMADD 3000
+#define NUMMAX 100
 
 using namespace std;
 
 int main() {
     
-    ofstream MyFile("insertions7.txt");
+    ofstream MyFile("insertDelete1.txt");
 
-    for (int i = 0; i < NUMADD; i++) {
+    for (int i = 1; i <= NUMADD; i++) {
         MyFile << 1 << " ";
-        MyFile << (rand() % NUMMAX) << "\n";
+        MyFile << i << "\n";
+
     }
+    for (int i = 1; i <= NUMADD; i++) {
+        MyFile << 2 << " ";
+        MyFile << i << "\n";
+
+    }
+    
     MyFile << 4 << "\n";
     MyFile << 5;
 
